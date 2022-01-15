82afd05f2bcf9dae1822c32d5a380d5ca177d119
Jan Jongboom <janjongboom@gmail.com>
2022-01-15 19:10:19 +0100

    Now properly pretrained model, converges a bit

--- run.sh
+++ run.sh
@@ -11,7 +11,7 @@ python3 -u extract_dataset.py --x-file /home/X_train_features_beer.npy --y-file 
 
 cd yolov5
 # --freeze 23 to do transfer learning?
-python3 -u train.py --img $IMAGE_SIZE --batch 16 --epochs 50 --data /tmp/data/data.yaml --cfg /tmp/data/yolov5s.yaml --weights '' --name yolov5s_results --cache
+python3 -u train.py --img $IMAGE_SIZE --batch 16 --epochs 200 --freeze 24 --data /tmp/data/data.yaml --weights ../yolov5s.pt --name yolov5s_results --cache
 python3 -u export.py --weights ./runs/train/yolov5s_results/weights/last.pt --img $IMAGE_SIZE --include tflite
 python3 -u export.py --weights ./runs/train/yolov5s_results/weights/last.pt --img $IMAGE_SIZE --include tflite --int8
 cp runs/train/yolov5s_results/weights/last-fp16.tflite /home/model.tflite
